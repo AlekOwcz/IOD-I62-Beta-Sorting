f82f59710fb916e3ff4264f22c18a13bb07e2856
Aleksander Owczarczak <96996586+AlekOwcz@users.noreply.github.com>
2022-12-18 15:47:56 -0800

    Add JSON parsing attempt

--- src/main/java/pl/put/poznan/sortingmadness/app/SortingMadnessApplication.java
+++ src/main/java/pl/put/poznan/sortingmadness/app/SortingMadnessApplication.java
@@ -10,55 +10,11 @@ import pl.put.poznan.sortingmadness.logic.SortingMadness;
 import java.util.Scanner;
 
 @SpringBootApplication(scanBasePackages = {"pl.put.poznan.sortingmadness.rest"})
-public class SortingMadnessApplication implements CommandLineRunner {
+public class SortingMadnessApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SortingMadnessApplication.class, args);
     }
-
-    @Override
-    public void run(String... args) {
-        SortingMadness Sorter = new SortingMadness();
-        Scanner myInput = new Scanner(System.in);
-
-        System.out.println("\n\n\nWelcome to Sorting Madness!");
-        System.out.println("What would you like to do?");
-        System.out.println("1. Sort a one dimensional array");
-        System.out.println("2. Sort an array of objects");
-        System.out.println(">");
-
-        String sortObject = myInput.nextLine();
-        switch(sortObject){
-            case "1":
-                Sorter.readOneDimArray();
-                break;
-            case "2":
-                Sorter.readObjectArray();
-                break;
-            default:
-                System.out.println("Unknown operation, try again.\n>");
-        }
-
-        System.out.println("Would you like to sort in ascending or descending order?\n>");
-        System.out.println("1. Ascending");
-        System.out.println("2. Descending");
-        Sorter.readOrder();
-
-        System.out.println("After how many iterations should the algorithm stop? (-1 to sort till the end)\n>");
-        Sorter.readIterations();
-
-        System.out.println("Which algorithms out of these do you want to use? (Example input 2, 4, 5)");
-        System.out.println("1. AUTO (Picks the best algorithm to sort the data with)");
-        System.out.println("2. INSERTION SORT");
-        System.out.println("3. SELECTION SORT");
-        System.out.println("4. BUBBLE SORT");
-        System.out.println("5. QUICK SORT");
-        System.out.println("6. MERGE SORT");
-        System.out.println("7. HEAP SORT");
-        System.out.println(">");
-        Sorter.readAlgorithm();
-
-        Sorter.sort();
 
     }
 }
---
+++ src/main/java/pl/put/poznan/sortingmadness/rest/JSONInputObj.java
@@ -0,0 +1,35 @@
+package pl.put.poznan.sortingmadness.rest;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class JSONInputObj {
+    @JsonProperty("algorithms")
+    private Object algorithms;
+
+    @JsonProperty("data")
+    private Object data;
+
+    @JsonProperty("attribute")
+    private String attribute;
+    public JSONInputObj(@JsonProperty("data") Object data, @JsonProperty("algorithms") Object algorithms, @JsonProperty("algorithms") String attribute) {
+        this.algorithms = algorithms;
+        this.data = data;
+        this.attribute = attribute;
+    }
+
+    public Object getAlgorithms() {
+        return algorithms;
+    }
+
+    public void setAlgorithms(Object algorithms) {
+        this.algorithms = algorithms;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+}
