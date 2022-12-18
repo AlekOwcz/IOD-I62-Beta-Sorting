9044541bebae953397dd0caa11e1d824286876c6
Aleksander Owczarczak <96996586+AlekOwcz@users.noreply.github.com>
2022-12-18 12:59:20 -0800

    Add files via upload

---
+++ src/main/java/pl/put/poznan/sortingmadness/logic/SortingStrategy.java
@@ -0,0 +1,8 @@
+package pl.put.poznan.sortingmadness.logic;
+
+public interface SortingStrategy {
+    //TODO DECIDE ON RETURN TYPE
+    //long time = -1;
+    //TODO MEMBER LIST FOR KEEPING THE RESULT
+    //public String sort();
+}
