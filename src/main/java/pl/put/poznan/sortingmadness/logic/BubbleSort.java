edc68a28fc67800698b21232a5455fd7390a97b9
AdamPiotrowsky <95354097+AdamPiotrowsky@users.noreply.github.com>
2022-12-21 01:25:48 +0100

    Buuble & Heap sort

--- src/main/java/pl/put/poznan/sortingmadness/logic/BubbleSort.java
+++ src/main/java/pl/put/poznan/sortingmadness/logic/BubbleSort.java
@@ -4,12 +4,28 @@ import java.util.ArrayList;
 import java.util.Comparator;
 
 public class BubbleSort<T extends  Comparable<T>> implements SortingStrategy<T> {
+
     Comparator<T> comparator;
+
     public BubbleSort(Comparator<T> comparator){
         this.comparator = comparator;
     }
+
     @Override
     public ArrayList<T> sort(ArrayList<T> data) {
-        return null;
+        return bubble(data);
+    }
+
+    private ArrayList<T> bubble(ArrayList<T> data){
+
+        int n = data.size();
+        for (int i = 0; i < n - 1; i++)
+            for (int j = 0; j < n - i - 1; j++)
+                if (comparator.compare(data.get(j) , data.get(j+1)) > 0) {
+                    T tmp = data.get(j);
+                    data.set(j, data.get(j+1));
+                    data.set(j+1, tmp);
+                }
+        return data;
     }
 }
--- src/main/java/pl/put/poznan/sortingmadness/logic/HeapSort.java
+++ src/main/java/pl/put/poznan/sortingmadness/logic/HeapSort.java
@@ -3,14 +3,52 @@ package pl.put.poznan.sortingmadness.logic;
 import java.util.ArrayList;
 import java.util.Comparator;
 
-public class HeapSort<T extends  Comparable<T>> implements SortingStrategy<T>  {
+public class HeapSort<T extends  Comparable<T>> implements SortingStrategy<T> {
 
     Comparator<T> comparator;
+
     public HeapSort(Comparator<T> comparator){
         this.comparator = comparator;
     }
+
     @Override
     public ArrayList<T> sort(ArrayList<T> data) {
-        return null;
+        return heap(data);
+    }
+
+    private ArrayList<T> heap(ArrayList<T> data)
+    {
+        int n = data.size();
+        for (int i = n / 2 - 1; i >= 0; i--)
+            heapify(data, n, i);
+
+        for (int i=n-1; i>=0; i--)
+        {
+            T tmp = data.get(0);
+            data.set(0, data.get(i));
+            data.set(i, tmp);
+            heapify(data, i, 0);
+        }
+        return data;
+    }
+
+    void heapify(ArrayList<T> data, int n, int i)
+    {
+        int largest = i;
+        int l = 2*i + 1;
+        int r = 2*i + 2;
+
+        if (l < n && comparator.compare(data.get(l) , data.get(largest)) > 0)
+            largest = l;
+
+        if (r < n && comparator.compare(data.get(r) , data.get(largest)) > 0)
+            largest = r;
+        if (largest != i)
+        {
+            T swap = data.get(i);
+            data.set(i, data.get(largest));
+            data.set(largest, swap);
+            heapify(data, n, largest);
+        }
     }
 }
